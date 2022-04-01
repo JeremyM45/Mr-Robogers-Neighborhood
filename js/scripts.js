77692cdcb88169c46ce927a17b15668c4ba8f2b7
JeremyM45 <jeremyvs1945@gmail.com>
2022-04-01 09:00:05 -0700

    add js logic to incriment array from 0 to input number

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,9 @@
 function boopBeep(string) {
-  let num = parseInt(string)
-  return num;
+  let num = parseInt(string);
+  const numArray = [];
+  for (i = 0; i <= num; i++)  {
+    number = i;
+    numArray.push(number);
+  }
+  return numArray;
 }
